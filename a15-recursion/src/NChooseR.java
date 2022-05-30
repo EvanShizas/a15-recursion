fe6a4b008ab9a051c37f2491eae6492685df92f2
Evan Shizas <shizasevan@gmail.com>
2022-05-30 00:05:07 -0400

    Final NChooseR...
    Should be the final version of program. Due to Java's limitations in storing large int values, I had to create Band-Aid solutions to allow for the program to work correctly. For example, the value of "n" cannot ever be smaller than "r", as it leads to value being calculated to be too large for it to be stored as a long data value (>9,223,372,036,854,775,807), causing incorrect results, but NO CRASHING (phewwww...). Also, the largest value that can be given in the program is 20! DO NOT MAKE IT LARGER, as Java CANNOT store that in a long data format and will cause it to create a "divide by 0" error (memory wipe).

--- a15-recursion/src/NChooseR.java
+++ a15-recursion/src/NChooseR.java
@@ -1,7 +1,7 @@
 /**
  * Calculates the number of ways r different objects can be chosen from a set of n objects.
  * 
- * modified     20220524
+ * modified     20220527
  * date         20220524
  * @filename    NChooseR.java
  * @author      Alvin Chan, Evan Shizas
@@ -22,6 +22,8 @@ import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.Color;
 
 public class NChooseR extends JFrame {
@@ -33,6 +35,8 @@ public class NChooseR extends JFrame {
 	private JSpinner objectsSelect;
 	private JSpinner chooseSelect;
 
+	int oldNum = 0;
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -66,16 +70,16 @@ public class NChooseR extends JFrame {
 		title.setBounds(0, 0, 475, 40);
 		contentPane.add(title);
 		
-		JLabel enterObjectsLbl = new JLabel("Enter the number of objects to choose (r): ");
+		JLabel enterObjectsLbl = new JLabel("Enter the number of objects to choose (r): [0 - 20]");
 		enterObjectsLbl.setBackground(Color.WHITE);
 		enterObjectsLbl.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		enterObjectsLbl.setBounds(10, 112, 310, 22);
+		enterObjectsLbl.setBounds(10, 112, 370, 22);
 		contentPane.add(enterObjectsLbl);
 		
-		JLabel enterChooseLbl = new JLabel("Enter the number of objects to choose from (n): ");
+		JLabel enterChooseLbl = new JLabel("Enter the number of objects to choose from (n): [r - 20]");
 		enterChooseLbl.setBackground(Color.WHITE);
 		enterChooseLbl.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		enterChooseLbl.setBounds(10, 147, 310, 22);
+		enterChooseLbl.setBounds(10, 147, 370, 22);
 		contentPane.add(enterChooseLbl);
 
 		nChooseRInfo = new JTextArea();
@@ -89,16 +93,22 @@ public class NChooseR extends JFrame {
 		contentPane.add(nChooseRInfo);
 
 		objectsSelect = new JSpinner();
-		objectsSelect.setModel(new SpinnerNumberModel(new Integer(0), null, null, new Integer(1)));
+		objectsSelect.setModel(new SpinnerNumberModel(0, 0, 20, 1));
 		objectsSelect.setForeground(Color.WHITE);
 		objectsSelect.setBackground(Color.WHITE);
-		objectsSelect.setBounds(318, 112, 147, 22);
+		objectsSelect.setBounds(390, 112, 75, 22);
 		contentPane.add(objectsSelect);
+		objectsSelect.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent evt) {
+				objectsSelectActionPerformed(evt);
+			}
+		});
 		
 		chooseSelect = new JSpinner();
+		chooseSelect.setModel(new SpinnerNumberModel(0, 0, 20, 1));
 		chooseSelect.setForeground(Color.WHITE);
 		chooseSelect.setBackground(Color.WHITE);
-		chooseSelect.setBounds(318, 147, 147, 22);
+		chooseSelect.setBounds(390, 147, 75, 22);
 		contentPane.add(chooseSelect);
 
 		calculate = new JButton("Calculate the Number of Ways");
@@ -118,8 +128,63 @@ public class NChooseR extends JFrame {
 		contentPane.add(dialogBox);
 		dialogBox.setColumns(10);
 	}
+	
+	private void objectsSelectActionPerformed(ChangeEvent e) { 
+		// Needed due to Java not being able to handle the large values if objectSelect has a value greater than that of chooseSelect!
+		
+		if (oldNum > (int) objectsSelect.getValue() || (int) chooseSelect.getValue() > (int) objectsSelect.getValue())
+			chooseSelect.setModel(new SpinnerNumberModel((int) chooseSelect.getValue(), (int) objectsSelect.getValue(), 20, 1));
+		else
+			chooseSelect.setModel(new SpinnerNumberModel((int) objectsSelect.getValue(), (int) objectsSelect.getValue(), 20, 1));
+		
+		oldNum = (int) objectsSelect.getValue();
+	}
 
-	private void calculateActionPerformed(java.awt.event.ActionEvent evt) {
-
+	private void calculateActionPerformed(java.awt.event.ActionEvent evt) {		
+		long r = (long)((int) objectsSelect.getValue()), n = (long)((int) chooseSelect.getValue()), numerator = 1, denominator1 = 1, denominator2 = 1, result = 0,
+				diffFactorial = n-r, i = n, i1 = r;
+		
+		result = numeratorRecur(numerator, i) / (denominator1Recur(denominator1, i1) * denominator2Recur(denominator2, diffFactorial));
+		
+		dialogBox.setText("There are " + result + " ways!");
+		
+		if (result == 1)
+			dialogBox.setText("There is " + result + " way!");
+	}
+	
+	public long numeratorRecur(long numerator, long i) {
+		if (i > 0) {
+			numerator *= i;
+			i--;
+			return numeratorRecur(numerator, i);
+		}
+		
+		else {
+			return numerator;
+		}
+	}
+	
+	public long denominator1Recur(long denominator1, long i1) {
+		if (i1 > 0) {
+			denominator1 *= i1;
+			i1--;
+			return denominator1Recur(denominator1, i1);
+		}
+		
+		else {
+			return denominator1;
+		}
+	}
+	
+	public long denominator2Recur(long denominator2, long diffFactorial) {
+		if (diffFactorial > 0) {
+			denominator2 *= diffFactorial;
+			diffFactorial--;
+			return denominator2Recur(denominator2, diffFactorial);
+		}
+		
+		else {
+			return denominator2;
+		}
 	}
 }
